1231451effb1e524a434d8638e8b22f873bd38fa
rd60 <rd60@ce-link.com>
2014-07-14 13:42:20 +0800

    add CEMainViewController

---
+++ LearnGit/LearnGit/CEMainViewController.h
@@ -0,0 +1,16 @@
+//
+//  CEMainViewController.h
+//  LearnGit
+//
+//  Created by rd60 on 14-7-14.
+//  Copyright (c) 2014年 celink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CEMainViewController : UIViewController
+{
+    
+}
+
+@end
---
+++ LearnGit/LearnGit/CEMainViewController.m
@@ -0,0 +1,49 @@
+//
+//  CEMainViewController.m
+//  LearnGit
+//
+//  Created by rd60 on 14-7-14.
+//  Copyright (c) 2014年 celink. All rights reserved.
+//
+
+#import "CEMainViewController.h"
+
+@interface CEMainViewController ()
+
+@end
+
+@implementation CEMainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

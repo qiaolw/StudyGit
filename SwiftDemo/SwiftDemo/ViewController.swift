79f31e6ba67734afd2462fd007cbd5d78c3afea2
rd60 <rd60@ce-link.com>
2017-07-23 10:35:43 +0800

    add MainFunction View

--- SwiftDemo/SwiftDemo/ViewController.swift
+++ SwiftDemo/SwiftDemo/ViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var mainFunctionList: [NSString] = ["Swift", "UI", "NetWork", "SQLite", "Tools"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        print(mainFunctionList.count)
+        return self.mainFunctionList.count
+    }
+    
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        print("cellForRowAt");
+        let cell:UITableViewCell = UITableViewCell(style:UITableViewCellStyle.default, reuseIdentifier: "MainFunctionIdentifier")
+        cell.textLabel!.text = mainFunctionList[indexPath.row] as String
+
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        print("didSelectRowAt");
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 
 }
-
